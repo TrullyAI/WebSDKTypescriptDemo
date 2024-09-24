c382f8a256d0bd58ac63245d19f0e8e997d0af47
anahiforesi <anahi.foresi@trully.ai>
2024-09-24 16:41:26 -0300

    demo v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# WebSDKDemo
+# Web SDK Typescript Demo
+
+This demo was created following the steps detailed in the
+[Docs](https://www.npmjs.com/package/@trully/trully-sdk-react)
+
+#### SDK version 1.0.0
+
+## How to use it
+
+1.  Clone this repository
+2.  Install dependencies (npm install)
+3.  Replace apiKey
+4.  Run (npm run dev)
+
+### ⚠️ Make sure you replace 'YOUR_API_KEY' with the apiKey provided by costumer services
---
+++ src/App.tsx
@@ -0,0 +1,144 @@
+import { DMResponse, TrullySdkWeb } from "@trully/trully-sdk-react";
+import { useState } from "react";
+
+function App() {
+  const [gettingResults, setGettingResults] = useState(true);
+  const [response, setResponse] = useState<DMResponse | null>(null);
+
+  const setLabelColor = (label?: string) => {
+    switch (label) {
+      case "potential threat":
+        return "red";
+      case "review":
+        return "yellow";
+      case "no threat":
+        return "green";
+      default:
+        return "";
+    }
+  };
+
+  const getImageUrl = (base64?: string) => {
+    if (!base64) return null;
+
+    const byteString = atob(base64 as string);
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const uintArray = new Uint8Array(arrayBuffer);
+
+    for (let i = 0; i < byteString.length; i++) {
+      uintArray[i] = byteString.charCodeAt(i);
+    }
+
+    const blob = new Blob([uintArray], { type: "image/png" });
+    const url = URL.createObjectURL(blob);
+    return url;
+  };
+
+  return (
+    <>
+      {gettingResults ? (
+        <TrullySdkWeb
+          configuration={{
+            isDev: true,
+            apiKey: "YOU_API_KEY",
+            user_id: "YOUR_USER_ID",
+            handleData: (response) => {
+              setGettingResults(() => {
+                setResponse(response);
+                return false;
+              });
+            },
+            handleError: (error) => {
+              console.log(error);
+            },
+          }}
+        />
+      ) : (
+        <div className="trully-container trully-response">
+          <img
+            src="https://trully-api-documentation.s3.amazonaws.com/trully-sdk/logo-trully.svg"
+            alt="logo"
+            className="trully-logo"
+          />
+          <div className="trully-data">
+            <div className="images-container">
+              <div className="image">
+                <h4>Frente recortado</h4>
+                <img
+                  src={getImageUrl(response?.document_image) ?? "./card.png"}
+                  className="trully-id"
+                  alt="icon"
+                />
+              </div>
+              <div className="image">
+                <h4>Frente completo</h4>
+                <img
+                  src={
+                    getImageUrl(response?.document_image_complete) ??
+                    "./card.png"
+                  }
+                  className="trully-id"
+                  alt="icon"
+                />
+              </div>
+              <div className="image">
+                <h4>Dorso recortado</h4>
+                <img
+                  src={
+                    getImageUrl(response?.document_image_back) ?? "./card.png"
+                  }
+                  className="trully-id"
+                  alt="icon"
+                />
+              </div>
+              <div className="image">
+                <h4>Dorso completo</h4>
+                <img
+                  src={
+                    getImageUrl(response?.document_image_back_complete) ??
+                    "./card.png"
+                  }
+                  className="trully-id"
+                  alt="icon"
+                />
+              </div>
+              <div className="image">
+                <h4>Selfie</h4>
+                <img
+                  src={getImageUrl(response?.image) ?? "./card.png"}
+                  className="trully-id"
+                  alt="icon"
+                />
+              </div>
+            </div>
+            <div className="info-container">
+              <div>
+                <h4>Label</h4>
+                <span
+                  className={`${setLabelColor(
+                    response?.label?.toLowerCase()
+                  )} trully-text`}
+                >
+                  {response?.label ?? "No encontrado"}
+                </span>
+              </div>
+            </div>
+            <button
+              className="trully-button trully-text"
+              onClick={() => {
+                setGettingResults(() => {
+                  setResponse(null);
+                  return true;
+                });
+              }}
+            >
+              Reintentar
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
